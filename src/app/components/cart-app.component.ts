9518436c54694fb7643b656a3e5e67fb3f12d775
Dani Te <daniel.te.mar@gmail.com>
2024-08-04 11:44:34 +0200

    Añadiendo cart component

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../models/product';
 import { CatalogComponent } from './catalog/catalog.component';
+import { CartComponent } from './cart/cart.component';
+import { CartItem } from '../models/cartItem';
 
 @Component({
   selector: 'app-cart-app',
   standalone: true,
-  imports: [CatalogComponent],
+  imports: [CatalogComponent,CartComponent],
   templateUrl: './cart-app.component.html',
   styleUrl: './cart-app.component.css'
 })
@@ -14,9 +16,12 @@ export class CartAppComponent implements OnInit{
 
   products: Product[] = [];
 
+  items: CartItem[] = [];
+
   constructor(private service: ProductService){
 
   }
+  
   ngOnInit(): void {
     this.products = this.service.findAll();
   }
